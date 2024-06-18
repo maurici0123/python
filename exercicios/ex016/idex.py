267b900818b23caf042c99384f0b88230794ae3d
mauricio <mauriciosilvajunior12@gmail.com>
2024-06-18 13:13:13 -0300

    gemini

--- exercicios/ex016/idex.py
+++ exercicios/ex016/idex.py
@@ -4,7 +4,7 @@ import pyautogui as pg
 time.sleep(5)
 pg.PAUSE = .2
 
-for c in range(0, 30):
+for c in range(0, 10):
     pg.click(x=773, y=1032)
-    pg.write('')
+    pg.write('mariana gosta de comer!')
     pg.press('enter')
