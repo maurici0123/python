da1e8a5065c2fdf6eae7392fa0c11dd3b29637bb
mauricio <mauriciosilvajunior12@gmail.com>
2024-09-10 11:45:28 -0300

    aa de sql

--- exercicios/test/test02/idex.py
+++ exercicios/test/test02/idex.py
@@ -1,4 +1,16 @@
-lista = [1, 2, 3, 4, 5]
+import mysql.connector
 
-for i in lista:
-    print(i)
+meudb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    database='aa'
+)
+
+cursor = meudb.cursor()
+
+cursor.execute('SELECT Artist, max(sales) FROM aa.money_makers_bb WHERE year = 2020')
+
+resultado = cursor.fetchall()
+
+print(resultado)
